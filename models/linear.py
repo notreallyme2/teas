86725e981cb8221bea817cd662a88f11576cedf8
Lindsay Edwards <lindsay.m.edwards@gsk.com>
2020-01-31 09:38:25 +0000

    Made a mess, cleaning it up. THe refactor didn't work, not all update_batch functions are the samegit add -A

--- models/linear.py
+++ models/linear.py
@@ -11,9 +11,28 @@ from torch import nn, optim, tensor, FloatTensor
 from torch.utils.data import Dataset, DataLoader, random_split
 import torch.nn.functional as F
 
-class ModelUtilities:
-    """This class holds methods that are common to every torch class / model in this module"""
-    
+class LinearMLP(nn.Module):
+    """A pytorch module to build a simple linear multilayer perceptron"""
+
+    def __init__(self, input_dim = 100, hidden_dim = 512, output_dim = 1000):
+        """
+        Parameters
+        ----------
+        input_dim : int
+            The number of input features
+        hidden_dim : int
+            The number of features in the hidden layer
+        output_dim : int
+            The number of output features
+        """
+        super().__init__()
+        self.input = nn.Linear(input_dim, hidden_dim)
+        self.output = nn.Linear(hidden_dim, output_dim)
+  
+    def forward(self, X):
+        X = self.input(X)
+        return (self.output(X))
+
     def update_batch(self, X, Y, optimizer, criterion, train = True):
         """update_batch takes a model, data, a learning rate and a boolean indicating whether this update 
         should be treated as a training run (i.e. the model's weights should be updated) 
@@ -40,50 +59,7 @@ class ModelUtilities:
             optimizer.zero_grad()
         return loss.item()
 
-class LinearMLP(nn.Module, ModelUtilities):
-    """A pytorch module to build a simple linear multilayer perceptron"""
-
-    def __init__(self, input_dim = 100, hidden_dim = 512, output_dim = 1000):
-        """
-        Parameters
-        ----------
-        input_dim : int
-            The number of input features
-        hidden_dim : int
-            The number of features in the hidden layer
-        output_dim : int
-            The number of output features
-        """
-        super().__init__()
-        self.input = nn.Linear(input_dim, hidden_dim)
-        self.output = nn.Linear(hidden_dim, output_dim)
-  
-    def forward(self, X):
-        X = self.input(X)
-        return (self.output(X))
-
-class MiniLinearMLP(nn.Module, ModelUtilities):
-    """A pytorch module to build an even simpler linear multilayer perceptron"""
-
-    def __init__(self, input_dim = 100, output_dim = 1000):
-        """
-        Parameters
-        ----------
-        input_dim : int
-            The number of input features
-        hidden_dim : int
-            The number of features in the hidden layer
-        output_dim : int
-            The number of output features
-        """
-        super().__init__()
-        self.input_output = nn.Linear(input_dim, output_dim)
-  
-    def forward(self, X):
-        return (self.input_output(X))
-
-
-class LinearAE(nn.Module, ModelUtilities):
+class LinearAE(nn.Module):
     """A pytorch module to build a simple linear autoencoder"""
 
     def __init__(self, input_dim = 100, hidden_dim = 512):
@@ -109,7 +85,30 @@ class LinearAE(nn.Module, ModelUtilities):
     def X_from_Z(self, Z):
         return self.output(Z)
 
-class LinearFEA(nn.Module, ModelUtilities):
+    def update_batch(self, X, optimizer, criterion, train = True):
+        """update_batch takes a model, data, a learning rate and a boolean indicating whether this update should be treated as a training run (i.e. the model's weights should be updated) 
+        or not. 
+
+        Parameters
+        ----------
+        model : torch.nn.mnodule
+            The model to be updated
+        X : torch.FloatTensor
+            The input data (i.e feature matrix)
+        lr : float
+            The learning rate to be passed to the optimizer
+        train : bool
+            Should the weights be updated (default = True)
+        """
+        X_tilde = self.forward(X)
+        loss = criterion(X_tilde, X)
+        if train:
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        return loss.item()
+
+class LinearFEA(nn.Module):
     """A pytorch module to build a linear forward-embedding autoencoder"""
 
     def __init__(self, input_dim = 100, hidden_dim = 512, output_dim = 1000):
@@ -133,8 +132,34 @@ class LinearFEA(nn.Module, ModelUtilities):
         Y_hat = self.predict_Y(Z)
         X_tilde = self.reconstruct_X(Z)
         return Y_hat, X_tilde
+    
+    def update_batch(self, X, Y, optimizer, criterion, train = True):
+        """update_batch takes a model, data, a learning rate and a boolean indicating whether this update 
+        should be treated as a training run (i.e. the model's weights should be updated) 
+        or not.  
+        
+        Parameters
+        ----------
+        model : torch.nn.mnodule
+            The model to be updated
+        X : torch.FloatTensor
+            The input data (i.e feature matrix)
+        Y : torch.FloatTensor
+            The target matrix)
+        lr : float
+            The learning rate to be passed to the optimizer
+        train : bool
+            Should the weights be updated (default = True)
+        """
+        Y_hat, X_tilde = self.forward(X)
+        loss = criterion(X, X_tilde, Y, Y_hat)
+        if train:
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        return loss.item()
 
-class LinearTEA(nn.Module, ModelUtilities):
+class LinearTEA(nn.Module):
     """A pytorch module to build a linear target-embedding autoencoder"""
 
     def __init__(self, input_dim=100, hidden_dim = 256, output_dim=1000):
@@ -164,4 +189,27 @@ class LinearTEA(nn.Module, ModelUtilities):
         Z_from_X = self.input_X(X)
         Y_hat = self.predict_Y(Z_from_X)
         return Y_hat
- 
+    
+    def update_batch(self, X, Y, optimizer, criterion, train = True):
+        """update_batch takes a model, data, a learning rate and a boolean indicating whether this update 
+        should be treated as a training run (i.e. the model's weights should be updated) 
+        or not.  
+
+        Parameters
+        ----------
+        model : torch.nn.mnodule
+            The model to be updated
+        X : torch.FloatTensor
+            The input data (i.e feature matrix)
+        Y : torch.FloatTensor
+            (The target matrix)
+        train : bool
+            Should the weights be updated (default = True)
+        """
+        Y_hat, Z, Z_hat = self.forward(X, Y)
+        loss = criterion(Y, Y_hat, Z, Z_hat)
+        if train:
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        return loss.item()
